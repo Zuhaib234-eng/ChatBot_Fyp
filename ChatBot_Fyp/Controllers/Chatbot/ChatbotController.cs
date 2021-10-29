916f7a9eca4d133b439be1605896fb681a7bc6af
Zuhaib Ahmed <Zuhaibqureshi.za@gmail.com>
2021-10-29 19:53:05 +0500

    Zuhaib Ahmed

---
+++ ChatBot_Fyp/Controllers/Chatbot/ChatbotController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot_Fyp.Controllers.Chatbot
+{
+    public class ChatbotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
