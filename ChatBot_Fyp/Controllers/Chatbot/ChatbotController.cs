7894c95c81fe3759fe0f22b0f6f9d37607ec7683
Zuhaib Ahmed <Zuhaibqureshi.za@gmail.com>
2021-12-17 12:58:00 +0500

    Zuhaib

--- ChatBot_Fyp/Controllers/Chatbot/ChatbotController.cs
+++ ChatBot_Fyp/Controllers/Chatbot/ChatbotController.cs
@@ -11,6 +11,71 @@ namespace ChatBot_Fyp.Controllers.Chatbot
 {
     public class ChatbotController : Controller
     {
+        //Data train
+        static List<DataModel> _train = new List<DataModel>();
+        public ChatbotController()
+        {
+            using (StreamReader r = new StreamReader("DataFiles/Data.json"))
+            {
+                string json = r.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<DataListModel>(json);
+                _train = items.DataList;
+            }
+        }
+        class ClassInfo
+        {
+            public ClassInfo(string Quest, List<String> trainDocs)
+            {
+                Name = Quest;
+                var features = trainDocs;
+                WordsCount = features.Count();
+                WordCount =
+                    features.GroupBy(x => x)
+                    .ToDictionary(x => x.Key, x => x.Count());
+                NumberOfDocs = trainDocs.Count;
+            }
+            public string Name { get; set; }
+            public int WordsCount { get; set; }
+            public Dictionary<string, int> WordCount { get; set; }
+            public int NumberOfDocs { get; set; }
+            public int NumberOfOccurencesInTrainDocs(String word)
+            {
+                if (WordCount.Keys.Contains(word)) return WordCount[word];
+                return 0;
+            }
+        }
+
+        class Classifier
+        {
+            List<ClassInfo> _classes;
+            int _countOfDocs;
+            int _uniqWordsCount;
+            public Classifier(List<DataModel> train)
+            {
+                _classes = train.GroupBy(x => x.Question).Select(g => new ClassInfo(g.Key, g.Select(x => x.Question).ToList())).ToList();
+                _countOfDocs = train.Count;
+                _uniqWordsCount = train.SelectMany(x => x.Question.Split(' ')).GroupBy(x => x).Count();
+            }
+
+            public double IsInClassProbability(string className, string text)
+            {
+                //var words = text.ExtractFeatures();
+                var classResults = _classes
+                    .Select(x => new
+                    {
+                        Result = Math.Pow(Math.E, Calc(x.NumberOfDocs, _countOfDocs, new List<string>(), x.WordsCount, x, _uniqWordsCount)),
+                        ClassName = x.Name
+                    });
+
+
+                return classResults.Single(x => x.ClassName == className).Result / classResults.Sum(x => x.Result);
+            }
+
+            private static double Calc(double dc, double d, List<String> q, double lc, ClassInfo @class, double v)
+            {
+                return Math.Log(dc / d) + q.Sum(x => Math.Log((@class.NumberOfOccurencesInTrainDocs(x) + 1) / (v + lc)));
+            }
+        }
         public IActionResult Index()
         {
             return View();
